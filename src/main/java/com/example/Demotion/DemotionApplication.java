9946a716a072c5d2a265a73e6e88979a47dcecc6
EUNSEMIN <dmstpals104@ajou.ac.kr>
2025-03-25 23:55:51 +0900

    add project

---
+++ src/main/java/com/example/Demotion/DemotionApplication.java
@@ -0,0 +1,13 @@
+package com.example.Demotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemotionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Demotion/DemotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Demotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
